1647c7a4c535e9fa777de8dc32e9e748f4631200
Raoul1996 <baobaozhou199606@gmail.com>
2017-09-11 09:29:52 +0800

    修改query

--- src/routes/order/procedure/model.js
+++ src/routes/order/procedure/model.js
@@ -39,16 +39,15 @@ export default modelExtend(modalModel, tableModel, alertModel, {
       yield put({type: 'saveQuery', payload})
     },
     * fetchTable({payload}, {call, select, put}) {
-      const {order_code} = yield select(({procedure}) => procedure)
-      console.log(order_code)
-      const data = yield call(getProcedureList, payload)
+      const {query} = yield select(({procedure}) => procedure)
+      const {id} = query
+      const data = yield call(getProcedureList, id)
       yield put({type: 'setTable', payload: data.procedures})
     },
     * create({payload}, {put, call, select}) {
-      const {order_code} = yield select(({procedure}) => procedure)
       const data = yield call(procedureAdd, payload)
       console.log(data)
-      dispatch({type: 'fetchTable', payload: order_code})
+      console.log('this is the create dispatch')
       message.success('创建成功')
       yield put({type: 'fetchTable', payload})
       yield put({type: 'hideModal'})
